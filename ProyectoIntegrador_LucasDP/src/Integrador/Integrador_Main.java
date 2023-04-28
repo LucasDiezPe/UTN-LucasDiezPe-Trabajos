66e05fe27270cf66785ec15744e9cf600251fad5
LucasDiezPe <lucas.diez.pena@gmail.com>
2023-04-27 23:15:12 -0300

    etapa 2

--- ProyectoIntegrador_LucasDP/src/Integrador/Integrador_Main.java
+++ ProyectoIntegrador_LucasDP/src/Integrador/Integrador_Main.java
@@ -8,12 +8,12 @@ public class Integrador_Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("Estoy andando");
+	/*System.out.println("Estoy andando");
 	System.out.println("llamando el Enum de Resultados: "+listaResultados.GANADOR);
 	Equipos prueba=new Equipos();
 	prueba.nombre="saca chispa";
 	prueba.descripcion="Es un objeto pureba de la clase Equipos";
-	System.out.println(prueba.getNombre()+" : "+prueba.getDescripcion());
+	System.out.println(prueba.getNombre()+" : "+prueba.getDescripcion());*/
 	
 	String RutaInfoOficial="C:\\Users\\Usuario\\Documents\\GitHub\\UTN-LucasDiezPe-Trabajos\\ProyectoIntegrador_LucasDP\\src\\Integrador\\Archivos\\resultados.csv";
 	
@@ -24,8 +24,10 @@ public class Integrador_Main {
 	
 	pronosticoApostado pronosticoApostado = new pronosticoApostado();
 	ArrayList<pronosticoApostado>apuestas=pronosticoApostado.leerApuestaCSV(RutaPronosticos);
-	int puntos=0;
-	int acierto=3;
+	int []puntos = {0,0,0};
+	int []aciertos = {0,0,0};
+	int correcto=3;
+	
 	
 	for (PartidoJugado partido : partidos) {
 	    for (pronosticoApostado apuesta : apuestas) {
@@ -35,19 +37,24 @@ public class Integrador_Main {
 	            String pronostico = apuesta.getResultadoEq2();
 	            String resultado = partido.getResultadoEq2();
 	            if (pronostico.equals(resultado)) {
-	               puntos+=acierto;
+	               
 	                System.out.println("El pronóstico para el partido " + 
 	                    partido.getNroPartido() + " de la ronda " + partido.getNroRonda() +
-	                    " fue correcto.");
+	                    " del apostador : "+apuesta.getApostador()+" fue correcto.");
+	                if(apuesta.getApostador().equals("Mariana")) {puntos[0]+=correcto;aciertos[0]++;}
+	                if(apuesta.getApostador().equals("Pedro")) {puntos[1]+=correcto;aciertos[1]++;}
 	            } else {
 	               
 	                System.out.println("El pronóstico para el partido " + 
 	                    partido.getNroPartido() + " de la ronda " + partido.getNroRonda() +
-	                    " fue incorrecto.");
-	            } System.out.println("los puntos obtenidos por el Apostador : "+apuesta.getApostador()+" han sido : "+puntos);
+	                    " del apostador : "+apuesta.getApostador()+" fue incorrecto.");
+	            } 
+	   
 	        }
-	    }
+	    }// System.out.println(" Mariana lleva acumulados "+puntos[0]+" puntos");
+	    //System.out.println(" Pedro lleva acumulados "+puntos[1]+" puntos");
 	}
-	//for (pronosticoApostado apuesta : apuestas) { System.out.println("los puntos obtenidos por el Apostador : "+apuesta.getApostador()+" han sido : "+puntos);}
+	System.out.println("\n El puntaje final de Mariana es : "+puntos[0]+" puntos ya que ha acertado "+aciertos[0]+" resultados \n");
+    System.out.println("\n El puntaje final de Pedro es: "+puntos[1]+" puntos ya que ha acertado "+aciertos[1]+" resultados \n");
 
 	}}
